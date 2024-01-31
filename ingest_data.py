4662dcbecd7e29d2c33ea01e2b1b77552aba1774
Alexander Gunawan <58327698+silversunr1se@users.noreply.github.com>
2024-01-31 18:18:02 +0800

    dbt

--- ingest_data.py
+++ ingest_data.py
@@ -18,15 +18,16 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
+    csv_name = params.csv
     
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
+    #if url.endswith('.csv.gz'):
+     #   csv_name = 'output.csv.gz'
+    ##else:
+      #  csv_name = 'output.csv'
 
-    os.system(f"wget {url} -O {csv_name}")
+    #os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
@@ -34,8 +35,8 @@ def main(params):
 
     df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    #df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    #df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
@@ -49,8 +50,8 @@ def main(params):
             
             df = next(df_iter)
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            #df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            #df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
             df.to_sql(name=table_name, con=engine, if_exists='append')
 
@@ -72,6 +73,7 @@ if __name__ == '__main__':
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
     parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--csv', required=False, help='name of the csv file')
 
     args = parser.parse_args()
 
